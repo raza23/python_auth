b52b6653ae67240cb5fd34e9a0d26ed148fab3e1
Raza <rtshareef@gmail.com>
2020-04-21 19:00:12 -0400

    auth messages

--- python_auth/python_auth/pyAuth/authApp/views.py
+++ python_auth/python_auth/pyAuth/authApp/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 
 
@@ -17,8 +18,11 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            messages.success(request, ('Nice'))
             return redirect('home')
         else:
+            messages.success(request, ('Try Again'))
+
             return redirect('login')
     else:
         return render(request, 'auth/login.html', {})
