e764ea4e3cdd36fdc098ea2768d8e7f290d7a26e
Raza <rtshareef@gmail.com>
2020-04-22 19:00:02 -0400

    user can register

--- python_auth/python_auth/pyAuth/authApp/urls.py
+++ python_auth/python_auth/pyAuth/authApp/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('login/', views.login_user, name="login"),
     path('logout/', views.logout_view, name="logout"),
+    path('register/', views.register_user, name="register"),
+
 ]
--- python_auth/python_auth/pyAuth/authApp/views.py
+++ python_auth/python_auth/pyAuth/authApp/views.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import SignUpForm
 # Create your views here.
 
 
@@ -33,3 +35,22 @@ def logout_view(request):
     messages.success(request, ('Logged Out'))
     return redirect('home')
     # Redirect to a success page.
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ('Registered'))
+            return redirect('home')
+
+    else:
+        form = SignUpForm()
+    context = {'form': form}
+
+    return render(request, 'auth/register.html', context)
